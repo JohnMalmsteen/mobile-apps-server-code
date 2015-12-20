1f993c2bc01436b6254bca290780196573b98560
JohnMalmsteen <g00305339@gmit.ie>
2015-12-20 14:45:05 +0000

    Added routes to check if a username already exists and add new users

--- userManagement.js
+++ userManagement.js
@@ -2,7 +2,7 @@ var mongodb = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
 var crypto = require('crypto');
-var md5sum = crypto.createHash('md5');
+
 
 var app = express();
 
@@ -58,27 +58,77 @@ router.route('/register/').post(function(req, res){
       //respond with a failure here;
       res.status(500).json({error: "Error with request, password must be at least 7 characters"});
    }
-   // else if(check if user already exists){
-   //    res.status(500).json({error: "User name already exists"});
-   // }
    else{
+      var md5sum = crypto.createHash('md5');
       var salt = crypto.randomBytes(64).toString('hex');
       var user = {username: req.body.username, salt: salt, password: md5sum.update(req.body.password + salt).digest('hex')};
 
-      var token = md5sum.update(crypto.randomBytes(64).toString('hex')).digest('hex');
-      session[username] = {token: token, timeoutTime: (new Date().getTime() + 1200000)};
+      MongoClient.connect(url, function(err, db){
+         if (err) {
+           console.log('Unable to connect to the mongoDB server. Error:', err);
+         } else {
+            var collection = db.collection('users');
 
-      res.status(200).json({username: user.username, token: token});
-      //put this into the database here
+            collection.findOne({username: user.username}, function(err, doc){
+               if(err){
+                  res.status(500).json({error: "error", message: err});
+               }else {
+                  if(doc === null){
+                     collection.insert(user,function (err, doc) {
+                     if (err) {
+                        // If it failed, return error
+                        res.status(500).json({error: "There was a problem adding the information to the database.", message: err});
+                        db.close();
+                     }
+                     else {
+                        var token = crypto.createHash('md5').update(crypto.randomBytes(64).toString('hex')).digest('hex');
+                        session[user.username] = {token: token, timeoutTime: (new Date().getTime() + 1200000)};
+                        res.status(200).json({username: user.username, token: token});
+                        db.close();
+                     }
+                    });
+                 }else{
+                    res.status(500).json({error: 'User with that name already exists'});
+                    db.close();
+                 }
+               }
+            });
+         }
+      });
    }
 });
 
-router.route('/user/').get(function(req, res){
+router.route('/user/:username').get(function(req, res){
    //this method should be used to ensure the username the user has entered doesn't exist
+   MongoClient.connect(url, function(err, db){
+      if(err){
+         res.status(500).json({error: 'Unable to connect to database', message: err});
+      }
+      else{
+         var collection = db.collection('users');
+
+         collection.findOne({username: req.params.username}, function(err, doc){
+            if(err){
+               res.status(500).json({error: err});
+            }else{
+               if(doc === null){
+                  res.status(200).json({exists: false});
+               }else{
+                  res.status(200).json({exists: true});
+               }
+            }
+            db.close();
+         });
+      }
+   });
 });
 
 router.route('/login/').get(function(req, res){
    //generate a token and return it to the user and put it on a dictionary of curently valid tokens to be used for accessing the data
+});
+
+router.route('/').get(function(req, res){
+   res.status(200).json({message: 'This is the Nebins Tower API'});
 });
 
 function sessionTimeout(){
@@ -92,3 +142,8 @@ function sessionTimeout(){
 }
 
 setInterval(sessionTimeout, 10000);
+
+app.use('/', router);
+
+app.listen(port);
+console.log('Server listening on port: ' + port);
