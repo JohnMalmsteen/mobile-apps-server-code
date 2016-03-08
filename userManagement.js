abaf3e4f9490c0947ba0c835f0d119d9ef14d5bd
JohnMalmsteen <g00305339@gmit.ie>
2016-03-08 17:14:42 +0000

    Many bug fixes and functionality updates, sessions are nolonger overwritten on subsequent log in requests, cleargin function for the sessions is fixed (20 min session time)

--- userManagement.js
+++ userManagement.js
@@ -2,7 +2,6 @@ var mongodb = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
 var crypto = require('crypto');
-var https = require('https');
 var http = require('http');
 var fs = require('fs');
 
@@ -10,10 +9,6 @@ var app = express();
 
 var session = {};
 
-var options = {
-  key: fs.readFileSync('sslcert/server.key'),
-  cert: fs.readFileSync('sslcert/server.crt')
-};
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -46,9 +41,10 @@ var url = 'mongodb://localhost:27017/nebinstower';
 var MongoClient = mongodb.MongoClient;
 
 router.route('/register/').post(function(req, res){
-   if(req.body.username === null || req.body.password === null || req.body.password.length < 7){
+   console.log(req.body.username + ' ' + req.body.password);
+   if(req.body.username === undefined || req.body.password === undefined || req.body.password.length < 7){
       //respond with a failure here;
-      res.status(500).json({error: "Error with request, password must be at least 7 characters"});
+      res.status(500).json({error: "Error with request, password must be at least 7 characters, must include {username, password}"});
    }
    else{
       var md5sum = crypto.createHash('md5');
@@ -117,42 +113,51 @@ router.route('/user/:username').get(function(req, res){
 
 router.route('/login/').post(function(req, res){
    //generate a token and return it to the user and put it on a dictionary of curently valid tokens to be used for accessing the data
-   var postedUsername = req.body.username;
-   var postedPassword = req.body.password;
+   if(req.body.username === undefined || req.body.password === undefined){
+      //respond with a failure here;
+      res.status(500).json({error: "Error with request, must include {username, password}"});
+   }else{
+       var postedUsername = req.body.username;
+       var postedPassword = req.body.password;
+       
+       MongoClient.connect(url, function(err, db){
+          if(err){
+             res.status(500).json({error: 'Unable to connect to database', message: err});
+          }
+          else{
+             var collection = db.collection('users');
 
-   MongoClient.connect(url, function(err, db){
-      if(err){
-         res.status(500).json({error: 'Unable to connect to database', message: err});
-      }
-      else{
-         var collection = db.collection('users');
-
-         collection.findOne({username: postedUsername}, function(err, doc){
-            if(err){
-               res.status(500).json({error: err});
-            }else{
-               if(doc === null){
-                  res.status(404).json({error: 'User does not exist'});
-                  db.close();
-               }
-               else{
-                  var md5sum = crypto.createHash('md5');
-                  var hashedPassword = md5sum.update(postedPassword + doc.salt).digest('hex');
-                  if(hashedPassword === doc.password){
-                     var token = crypto.createHash('md5').update(crypto.randomBytes(64).toString('hex')).digest('hex');
-                     session[postedUsername] = {token: token, timeoutTime: (new Date().getTime() + 1200000)};
-                     res.status(200).json({token: token});
-                     db.close();
-                  }
-                  else{
-                     res.status(400).json({error: "Password Incorrect"});
-                     db.close();
-                  }
-               }
-            }
-         });
-      }
-   });
+             collection.findOne({username: postedUsername}, function(err, doc){
+                if(err){
+                   res.status(500).json({error: err});
+                }else{
+                   if(doc === null){
+                      res.status(404).json({error: 'User does not exist'});
+                      db.close();
+                   }
+                   else{
+                      var md5sum = crypto.createHash('md5');
+                      var hashedPassword = md5sum.update(postedPassword + doc.salt).digest('hex');
+                      if(hashedPassword === doc.password){
+                         if(session[postedUsername] !== null & session[postedUsername] !== undefined){
+                            res.status(200).json({token: session[postedUsername].token});
+                         }else{
+                             var token = crypto.createHash('md5').update(crypto.randomBytes(64).toString('hex')).digest('hex');
+                             session[postedUsername] = {token: token, timeoutTime: (new Date().getTime() + 1200000)};
+                             res.status(200).json({token: token});
+                         }
+                         db.close();
+                      }
+                      else{
+                         res.status(400).json({error: "Password Incorrect"});
+                         db.close();
+                      }
+                   }
+                }
+             });
+          }
+       });
+   }
 });
 
 router.route('/saveCharacters/').post(function(req, res){
@@ -216,10 +221,10 @@ router.route('/').get(function(req, res){
 
 function sessionTimeout(){
    var currentTime = new Date().getTime();
-   //console.log("clearing");
+   console.log("clearing");
    for(var user in session){
-      if(user.timeoutTime <= currentTime){
-         delete session.user;
+      if(session[user].timeoutTime <= currentTime){
+         delete session[user];
       }
    }
 }
@@ -228,7 +233,5 @@ setInterval(sessionTimeout, 10000);
 
 app.use('/', router);
 
-https.createServer(options, app).listen(port);
-http.createServer(app).listen(5001);
-console.log('HTTPS Server listening on port: ' + port);
-console.log('HTTP Server listening on port: ' + 5001);
+http.createServer(app).listen(port);
+console.log('HTTP Server listening on port: ' + port);
